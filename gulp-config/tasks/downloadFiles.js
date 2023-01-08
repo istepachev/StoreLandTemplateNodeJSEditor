5c7aea4f6de1060bc8d3f6678c33e868472c000e
Дмитрий <shcheklein.dev@gmail.com>
2023-01-08 11:17:18 +0300

    Fix: downloadFiles

--- gulp-config/tasks/downloadFiles.js
+++ gulp-config/tasks/downloadFiles.js
@@ -1,95 +1,99 @@
 import { Paths, FilesMap, URL_MAP } from "../const.js";
 import { SECRET_KEY } from "./config-check.js";
-import { URLSearchParams } from "node:url";
-import fs from "node:fs";
+import * as fs from "node:fs";
 import path from "node:path";
-import fetch from "node-fetch";
 import chalk from "chalk";
+import got from "got";
+import { FormData } from "formdata-node";
+import { deleteSync } from "del";
 
-function downloadFiles(done) {
+async function downloadFiles(done) {
   const FILES_PATH = `${Paths.downloadDir}`;
-  const FETCH_PARAMS = {
-    method: "post",
-    body: new URLSearchParams({ secret_key: SECRET_KEY }),
-    timeout: 10000,
+  deleteSync(FILES_PATH);
+
+  if (!fs.existsSync(FILES_PATH)) {
+    fs.mkdirSync(FILES_PATH);
+  }
+
+  const formData = new FormData();
+  formData.set("secret_key", SECRET_KEY);
+
+  const OPTIONS = {
+    body: formData,
+    timeout: {
+      send: 10000,
+    },
   };
 
-  fetch(URL_MAP.get_list, FETCH_PARAMS)
-    .then((res) => res.json())
-    .then((json) => {
-      if (json.status === `ok`) {
-        console.log(chalk.greenBright(`Загружен список всех файлов ✔️`));
-        if (!fs.existsSync(FILES_PATH)) {
-          fs.mkdirSync(FILES_PATH);
-        }
-        return json.data.map((item) => ({
-          file_id: item["file_id"]["value"],
-          file_name: item["file_name"]["value"],
-        }));
-      } else if (json.status === `error`) {
-        console.log(chalk.redBright(`Ошибка загрузки списка файлов ⛔`));
+  async function getFiles() {
+    const jsonFiles = await got.post(URL_MAP.get_list, OPTIONS).json();
+    const files = jsonFiles.data.map(({ file_id, file_name }) => ({
+      file_id: file_id.value,
+      file_name: file_name.value,
+    }));
+
+    console.log(
+      chalk.greenBright(
+        `Загружен список всех файлов ✔️\nВсего файлов ${files.length} шт.`
+      )
+    );
+
+    return files;
+  }
+  const files = await getFiles();
+  const filesLength = files.length;
+
+  getFile(files);
+
+  async function getFile(filesArray, count = 1) {
+    if (!filesArray.length) {
+      console.log(`Всего скачано файлов ${count} из ${filesLength}`);
+      done();
+
+      return;
+    }
+
+    const { file_id, file_name } = filesArray.shift();
+
+    try {
+      const jsonFile = await got
+        .post(`${URL_MAP.get_file}/${file_id}`, OPTIONS)
+        .json();
+
+      if (jsonFile.status === "error") {
+        console.log(chalk.redBright(`Ошибка загрузки ⛔: ${jsonFile.message}`));
       }
-    })
-    .then((array) => {
-      const arrLength = array.length;
-      let count = 1;
-      console.log(chalk.greenBright(`Всего файлов ${arrLength} шт.`));
 
-      const getFile = (filesArray) => {
-        if (!filesArray.length) {
-          console.log(`Всего скачано файлов ${count} из ${arrLength}`);
-          done();
+      const file = jsonFile["data"]["file_name"].value;
+      const fileContent = jsonFile["data"]["file_content"].value;
+      const fileExt = path.extname(file).replace(".", "");
 
-          return;
+      const fileDirName =
+        Object.keys(FilesMap).find((key) => FilesMap[key].includes(fileExt)) ||
+        "";
+      const newDir = `${FILES_PATH}/${fileDirName}`;
+
+      !fs.existsSync(newDir) && fs.mkdirSync(newDir);
+
+      fs.writeFile(`${newDir}/${file}`, fileContent, "base64", (err) => {
+        if (err) {
+          console.error(err);
         }
-        const { file_id, file_name } = filesArray.shift();
 
-        fetch(`${URL_MAP.get_file}/${file_id}`, FETCH_PARAMS)
-          .then((res) => res.json())
-          .then((json) => {
-            if (json.status === `ok`) {
-              const file = json["data"]["file_name"].value;
-              const fileContent = json["data"]["file_content"].value;
-              const fileExt = path.extname(file).replace(".", "");
-
-              const fileDirName =
-                Object.keys(FilesMap).find((key) =>
-                  FilesMap[key].includes(fileExt)
-                ) || "";
-              const newDir = `${FILES_PATH}/${fileDirName}`;
-
-              !fs.existsSync(newDir) && fs.mkdirSync(newDir);
-
-              fs.writeFile(
-                `${newDir}/${file}`,
-                fileContent,
-                "base64",
-                (err) => {
-                  if (err) {
-                    console.log(err);
-                  }
-
-                  console.log(
-                    `Скачан файл ${chalk.gray(
-                      file_name
-                    )}. Всего ${count} из ${arrLength}`
-                  );
-                  if (filesArray.length) {
-                    count++;
-                  }
-                  getFile(array);
-                }
-              );
-            } else if (json.status === `error`) {
-              console.log(
-                chalk.redBright(`Ошибка загрузки⛔: ${json.message}`)
-              );
-            }
-          })
-          .catch(console.log);
-      };
-      getFile(array);
-    });
+        console.log(
+          `Скачан файл ${chalk.greenBright(
+            file_name
+          )}. Всего ${count} из ${filesLength}`
+        );
+        if (filesArray.length) {
+          count++;
+        }
+        getFile(filesArray, count);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default downloadFiles;
