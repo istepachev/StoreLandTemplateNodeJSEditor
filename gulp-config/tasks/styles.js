6331e76b9501308d55a8f59eb61f175f7c825463
Dmitry <sheklein.dima@gmail.com>
2023-01-09 20:23:44 +0300

    Fix: styles.js

--- gulp-config/tasks/styles.js
+++ gulp-config/tasks/styles.js
@@ -10,10 +10,9 @@ import { Paths, BASE_DIR, preprocessor, preprocessorOn } from "../const.js";
 import { checkBuild } from "../utils.js";
 // import { browserSync } from "./browsersync.js";
 
-async function styles(_, filePath) {
-  const isBuild = checkBuild(filePath);
-  const file = filePath;
-  const fileName = !isBuild ? path.basename(file) : "";
+async function styles(evt, filePath) {
+  const isBuild = checkBuild(evt);
+  const fileName = !isBuild ? path.basename(filePath) : "";
 
   if (preprocessorOn) {
     const PATH = !isBuild
@@ -35,49 +34,8 @@ async function styles(_, filePath) {
           includePaths: [`${BASE_DIR}/${preprocessor}/templates/`],
         }).on("error", scss.logError)
       )
-      .pipe(
-        autoprefixer({
-          grid: true,
-          overrideBrowserslist: ["last 8 versions"],
-          browsers: [
-            "Android >= 4",
-            "Chrome >= 20",
-            "Firefox >= 24",
-            "Explorer >= 11",
-            "iOS >= 6",
-            "Opera >= 12",
-            "Safari >= 6",
-          ],
-        })
-      )
-      .pipe(
-        cleancss({
-          format: {
-            breaks: {
-              // controls where to insert breaks
-              afterAtRule: false, // controls if a line break comes after an at-rule; e.g. `@charset`; defaults to `false`
-              afterBlockBegins: true, // controls if a line break comes after a block begins; e.g. `@media`; defaults to `false`
-              afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
-              afterComment: true, // controls if a line break comes after a comment; defaults to `false`
-              afterProperty: false, // controls if a line break comes after a property; defaults to `false`
-              afterRuleBegins: false, // controls if a line break comes after a rule begins; defaults to `false`
-              afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
-              beforeBlockEnds: true, // controls if a line break comes before a block ends; defaults to `false`
-              betweenSelectors: false, // controls if a line break comes between selectors; defaults to `false`
-            },
-            breakWith: "\n", // controls the new line character, can be `'\r\n'` or `'\n'` (aliased as `'windows'` and `'unix'` or `'crlf'` and `'lf'`); defaults to system one, so former on Windows and latter on Unix
-            indentBy: 0, // controls number of characters to indent with; defaults to `0`
-            indentWith: "space", // controls a character to indent with, can be `'space'` or `'tab'`; defaults to `'space'`
-            spaces: {
-              // controls where to insert spaces
-              aroundSelectorRelation: true, // controls if spaces come around selector relations; e.g. `div > a`; defaults to `false`
-              beforeBlockBegins: true, // controls if a space comes before a block begins; e.g. `.block {`; defaults to `false`
-              beforeValue: true, // controls if a space comes before a value; e.g. `width: 1rem`; defaults to `false`
-            },
-            wrapAt: false, // controls maximum line length; defaults to `false`
-          },
-        })
-      )
+      .pipe(autoprefixer(getAutoprefixerConfig()))
+      .pipe(cleancss(getCleanCssConfig()))
       // .pipe(sourcemaps.write("/src/scss/sourcemaps/"))
       .pipe(replacePath("/src/scss/", ""))
       .pipe(dest(Paths.buildStatic));
@@ -85,6 +43,7 @@ async function styles(_, filePath) {
     if (isBuild) {
       return src([`${BASE_DIR}/css/**/*.css`])
         .pipe(replacePath(`/src/css/default`, ""))
+        .pipe(replacePath(`/css/default`, ""))
         .pipe(dest(Paths.buildStatic));
     } else {
       return (
@@ -96,7 +55,51 @@ async function styles(_, filePath) {
       );
     }
   }
-  isBuild && filePath();
+}
+
+function getAutoprefixerConfig() {
+  return {
+    grid: true,
+    overrideBrowserslist: ["last 8 versions"],
+    browsers: [
+      "Android >= 4",
+      "Chrome >= 20",
+      "Firefox >= 24",
+      "Explorer >= 11",
+      "iOS >= 6",
+      "Opera >= 12",
+      "Safari >= 6",
+    ],
+  };
+}
+
+function getCleanCssConfig() {
+  return {
+    format: {
+      breaks: {
+        // controls where to insert breaks
+        afterAtRule: false, // controls if a line break comes after an at-rule; e.g. `@charset`; defaults to `false`
+        afterBlockBegins: true, // controls if a line break comes after a block begins; e.g. `@media`; defaults to `false`
+        afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
+        afterComment: true, // controls if a line break comes after a comment; defaults to `false`
+        afterProperty: false, // controls if a line break comes after a property; defaults to `false`
+        afterRuleBegins: false, // controls if a line break comes after a rule begins; defaults to `false`
+        afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
+        beforeBlockEnds: true, // controls if a line break comes before a block ends; defaults to `false`
+        betweenSelectors: false, // controls if a line break comes between selectors; defaults to `false`
+      },
+      breakWith: "\n", // controls the new line character, can be `'\r\n'` or `'\n'` (aliased as `'windows'` and `'unix'` or `'crlf'` and `'lf'`); defaults to system one, so former on Windows and latter on Unix
+      indentBy: 0, // controls number of characters to indent with; defaults to `0`
+      indentWith: "space", // controls a character to indent with, can be `'space'` or `'tab'`; defaults to `'space'`
+      spaces: {
+        // controls where to insert spaces
+        aroundSelectorRelation: true, // controls if spaces come around selector relations; e.g. `div > a`; defaults to `false`
+        beforeBlockBegins: true, // controls if a space comes before a block begins; e.g. `.block {`; defaults to `false`
+        beforeValue: true, // controls if a space comes before a value; e.g. `width: 1rem`; defaults to `false`
+      },
+      wrapAt: false, // controls maximum line length; defaults to `false`
+    },
+  };
 }
 
 export default styles;
