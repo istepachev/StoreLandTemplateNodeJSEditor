961a599f6a01ada0d0e115c9f1fea79dd31b80a2
Dmitry <sheklein.dima@gmail.com>
2023-01-09 20:20:36 +0300

    Fix: fonts.js

--- gulp-config/tasks/fonts.js
+++ gulp-config/tasks/fonts.js
@@ -1,11 +1,12 @@
 import { src, dest } from "../../gulpfile.js";
-import { Paths } from "../const.js";
+import { Paths, FilesMap } from "../const.js";
 import { checkBuild } from "../utils.js";
+import plumber from "gulp-plumber";
 
 async function fonts(filePath = "") {
   return src(
     checkBuild(filePath)
-      ? `src/fonts/**/*.{${Paths.getFilesStr("fonts")}}`
+      ? `src/fonts/**/*.{${FilesMap.getFilesStr("fonts")}}`
       : filePath
   )
     .pipe(plumber())
